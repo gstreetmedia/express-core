2ef43939b369b061f8b58f59f0c3675f6b7b2a50
Andy Greenstreet <andrew@gstreetmedia.com>
2019-02-19 16:49:35 -0800

    lots of fixes post our first push

---
+++ schema/fields/fields-fields.js
@@ -0,0 +1,115 @@
+exports.admin = {
+	index : [
+		'id',
+		'adminForm',
+		'adminIndex',
+		'adminRead',
+		'dataSource',
+		'publicForm',
+		'publicIndex',
+		'publicRead',
+		'status',
+		'tableName',
+		'title',
+		'createdAt',
+		'updatedAt'
+	],
+	form : [
+		{
+			title:'Fields',
+			properties:[
+				'id',
+				'adminForm',
+				'adminIndex',
+				'adminRead',
+				'dataSource',
+				'publicForm',
+				'publicIndex',
+				'publicRead',
+				'status',
+				'tableName',
+				'title',
+				'createdAt',
+				'updatedAt'
+			]
+		}
+	],
+	read : [
+		{
+			title:'Fields',
+			properties:[
+				'id',
+				'adminForm',
+				'adminIndex',
+				'adminRead',
+				'dataSource',
+				'publicForm',
+				'publicIndex',
+				'publicRead',
+				'status',
+				'tableName',
+				'title',
+				'createdAt',
+				'updatedAt'
+			]
+		}
+	]
+}
+
+exports.public = {
+	index : [
+		'id',
+		'adminForm',
+		'adminIndex',
+		'adminRead',
+		'dataSource',
+		'publicForm',
+		'publicIndex',
+		'publicRead',
+		'status',
+		'tableName',
+		'title',
+		'createdAt',
+		'updatedAt'
+	],
+	form : [
+		{
+			title:'Fields',
+			properties:[
+				'id',
+				'adminForm',
+				'adminIndex',
+				'adminRead',
+				'dataSource',
+				'publicForm',
+				'publicIndex',
+				'publicRead',
+				'status',
+				'tableName',
+				'title',
+				'createdAt',
+				'updatedAt'
+			]
+		}
+	],
+	read : [
+		{
+			title:'Fields',
+			properties:[
+				'id',
+				'adminForm',
+				'adminIndex',
+				'adminRead',
+				'dataSource',
+				'publicForm',
+				'publicIndex',
+				'publicRead',
+				'status',
+				'tableName',
+				'title',
+				'createdAt',
+				'updatedAt'
+			]
+		}
+	]
+}
