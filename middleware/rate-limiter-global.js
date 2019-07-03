baa3cb2efc85380b0459ed5f1b35b5618efd1827
Andy Greenstreet <andrew@gstreetmedia.com>
2019-07-03 14:32:41 -0700

    fix rate limiters

--- middleware/rate-limiter-global.js
+++ middleware/rate-limiter-global.js
@@ -1,31 +1,38 @@
 const redis = require('redis');
 const {RateLimiterRedis: RateLimiterGlobal} = require('rate-limiter-flexible');
 const connectionStringParser = require("../helper/connection-string-parser");
+let rateLimiterMiddleware;
 
-let connection = connectionStringParser(process.env.CACHE_REDIS);
+if (process.env.CACHE_REDIS) {
 
-console.log(connection);
+	let connection = connectionStringParser(process.env.CACHE_REDIS);
 
-const redisClient = redis.createClient({
-	host: connection.host,
-	port: connection.port,
-	enable_offline_queue: false,
-});
+	const redisClient = redis.createClient({
+		host: connection.host,
+		port: connection.port,
+		enable_offline_queue: false,
+	});
 
-const rateLimiter = new RateLimiterGlobal({
-	redis: redisClient,
-	keyPrefix: 'middleware',
-	points: 10, // 10 requests
-	duration: 5, // per 1 second by IP
-});
+	const rateLimiter = new RateLimiterGlobal({
+		redis: redisClient,
+		keyPrefix: 'middleware',
+		points: 10, // 10 requests
+		duration: 5, // per 1 second by IP
+	});
 
-const rateLimiterMiddleware = async (req, res, next) => {
-	try {
-		await rateLimiter.consume(req.ip);
+	rateLimiterMiddleware = async (req, res, next) => {
+		try {
+			await rateLimiter.consume(req.ip);
+			next();
+		} catch(e) {
+			res.status(429).send('Too Many Requests');
+		}
+	};
+
+} else {
+	rateLimiterMiddleware = async (req, res, next) => {
 		next();
-	} catch(e) {
-		res.status(429).send('Too Many Requests');
 	}
-};
+}
 
 module.exports = rateLimiterMiddleware;
