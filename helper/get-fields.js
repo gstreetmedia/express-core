b1dbcd6a9d6cb0608d79604524752a95659a9fb3
Andy Greenstreet <andrew@gstreetmedia.com>
2019-10-16 13:08:50 -0700

    no need to cache. require is fast enough

--- helper/get-fields.js
+++ helper/get-fields.js
@@ -1,9 +1,5 @@
 let inflector = require("./inflector");
 
 module.exports = (tableName) => {
-	if (global.fieldCache[tableName]) {
-		return global.fieldCache[tableName];
-	}
-	global.fieldCache[tableName] = require(global.appRoot + '/src/schema/fields/' + inflector.dasherize(tableName.toLowerCase()) + '-schema');
-	return global.fieldCache[tableName];
+	return require(global.appRoot + '/src/schema/fields/' + inflector.dasherize(tableName.toLowerCase()) + '-fields')
 }
--- helper/get-schema.js
+++ helper/get-schema.js
@@ -1,10 +1,5 @@
 let inflector = require("./inflector");
 
 module.exports = (tableName) => {
-	if (global.schemaCache[tableName]) {
-		return global.schemaCache[tableName];
-	}
-	console.log("WTF loading schema " + tableName);
-	global.schemaCache[tableName] = require(global.appRoot + '/src/schema/' + inflector.dasherize(tableName.toLowerCase()) + '-schema');
-	return global.schemaCache[tableName];
+	return require(global.appRoot + '/src/schema/' + inflector.dasherize(tableName.toLowerCase()) + '-schema');
 }
