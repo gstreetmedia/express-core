2ca5a50fb586245e25e00c1bcad5d7682a24b7db
Andy Greenstreet <andrew@gstreetmedia.com>
2019-11-15 14:43:12 -0800

    testing

--- helper/cache-manager.js
+++ helper/cache-manager.js
@@ -32,10 +32,10 @@ if (!manager) {
 			console.log(error);
 		});
 
-		manager.get("last run", (err, value)=> {
+		manager.get(cachePrefix + "_" + "last_run", (err, value)=> {
 			manager.set(cachePrefix + "_" + "last_run", new Date().getTime());
 			console.log("last run => " + value);
-		})
+		});
 
 	} else {
 
